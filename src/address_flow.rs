84353dec4c70888e015c34ac6640eb0127bf3ec0
supertypo <suprtypo@pm.me>
2024-12-14 14:25:41 +0100

    Support for querying version and addresses through command line flags

--- src/address_flow.rs
+++ src/address_flow.rs
@@ -1,86 +1,109 @@
-use kaspa_p2p_lib::{ConnectionInitializer, IncomingRoute, KaspadHandshake, KaspadMessagePayloadType, Router};
-use std::sync::Arc;
-use std::time::SystemTime;
+use crate::Cli;
 use kaspa_p2p_lib::common::ProtocolError;
+use kaspa_p2p_lib::pb::kaspad_message::Payload;
 use kaspa_p2p_lib::pb::{KaspadMessage, VersionMessage};
-use log::{debug, trace, warn};
+use kaspa_p2p_lib::{ConnectionInitializer, IncomingRoute, KaspadHandshake, KaspadMessagePayloadType, Router};
+use std::sync::{Arc, RwLock};
+use std::time::{SystemTime, UNIX_EPOCH};
+use tokio::sync::mpsc::Sender;
 use tonic::async_trait;
 use uuid::Uuid;
 
-pub struct AddressesFlow {
-    receiver: IncomingRoute,
-    router: Arc<Router>,
+pub static ROUTER: RwLock<Option<Arc<Router>>> = RwLock::new(None);
+
+pub struct Initializer {
+    cli_args: Arc<Cli>,
+    sender: Sender<KaspadMessage>,
 }
 
-impl AddressesFlow {
-    pub async fn register(router: Arc<Router>) {
-        trace!("Subscribe to addresses");
-        let receiver = router.subscribe(vec![
-            KaspadMessagePayloadType::InvRelayBlock, // Mandatory for some reason
-            KaspadMessagePayloadType::Addresses,
-            KaspadMessagePayloadType::RequestAddresses,
-        ]);
-        let mut echo_flow = AddressesFlow { router, receiver };
-        debug!("Start receiving loop");
-        tokio::spawn(async move {
-            debug!("EchoFlow, start message dispatching loop");
-            while let Some(msg) = echo_flow.receiver.recv().await {
-                if !echo_flow.call(msg).await {
-                    warn!("EchoFlow, receive loop - call failed");
-                    break;
-                }
-            }
-            debug!("EchoFlow, exiting message dispatch loop");
-        });
-    }
-
-    async fn call(&self, msg: KaspadMessage) -> bool {
-        debug!("EchoFlow, got message:{:?}", msg);
-        self.router.enqueue(msg).await.is_ok()
-    }
-}
-
-#[derive(Default)]
-pub struct AddressesFlowInitializer {}
-
-fn build_dummy_version_message() -> VersionMessage {
-    VersionMessage {
-        protocol_version: 6,
-        services: 0,
-        timestamp: SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_millis() as i64,
-        address: None,
-        id: Vec::from(Uuid::new_v4().as_bytes()),
-        user_agent: String::new(),
-        disable_relay_tx: true,
-        subnetwork_id: None,
-        network: "kaspa-mainnet".to_string(),
-    }
-}
-
-impl AddressesFlowInitializer {
-    pub fn new() -> Self {
-        AddressesFlowInitializer {}
+impl Initializer {
+    pub fn new(cli_args: Arc<Cli>, sender: Sender<KaspadMessage>) -> Self {
+        Initializer { cli_args, sender }
     }
 }
 
 #[async_trait]
-impl ConnectionInitializer for AddressesFlowInitializer {
+impl ConnectionInitializer for Initializer {
     async fn initialize_connection(&self, router: Arc<Router>) -> Result<(), ProtocolError> {
-        // Build the handshake object and subscribe to handshake messages
+        ROUTER.write().unwrap().replace(router.clone());
         let mut handshake = KaspadHandshake::new(&router);
-        // We start the router receive loop only after we registered to handshake routes
         router.start();
-        // Build the local version message
-        let self_version_message = build_dummy_version_message();
-        // Perform the handshake
-        let peer_version_message = handshake.handshake(self_version_message).await?;
-        debug!("Handshake: {:?}", peer_version_message);
-        // Subscribe to messages
-        AddressesFlow::register(router.clone()).await;
-        // Send a ready signal
+        let version_msg = handshake.handshake(build_dummy_version_message(self.cli_args.clone())).await?;
+        self.sender.send(KaspadMessage { request_id: 0, response_id: 0, payload: Some(Payload::Version(version_msg)) }).await.unwrap();
+
+        let mut incoming_route = subscribe_all(&router);
+        let sender = self.sender.clone();
+        tokio::spawn(async move {
+            while let Some(msg) = incoming_route.recv().await {
+                if !sender.send(msg).await.is_ok() {
+                    break;
+                }
+            }
+        });
+
         handshake.exchange_ready_messages().await?;
-        // Note: at this point receivers for handshake subscriptions
-        // are dropped, thus effectively unsubscribing
         Ok(())
     }
+}
+
+fn build_dummy_version_message(cli_args: Arc<Cli>) -> VersionMessage {
+    VersionMessage {
+        protocol_version: 6,
+        services: 0,
+        timestamp: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis() as i64,
+        address: None,
+        id: Vec::from(Uuid::new_v4().as_bytes()),
+        user_agent: "kp2p".to_string(),
+        disable_relay_tx: true,
+        subnetwork_id: None,
+        network: format!("kaspa-{}", cli_args.network.to_lowercase()),
+    }
+}
+
+fn subscribe_all(router: &Arc<Router>) -> IncomingRoute {
+    router.subscribe(vec![
+        KaspadMessagePayloadType::Addresses,
+        KaspadMessagePayloadType::Block,
+        KaspadMessagePayloadType::Transaction,
+        KaspadMessagePayloadType::BlockLocator,
+        KaspadMessagePayloadType::RequestAddresses,
+        KaspadMessagePayloadType::RequestRelayBlocks,
+        KaspadMessagePayloadType::RequestTransactions,
+        KaspadMessagePayloadType::IbdBlock,
+        KaspadMessagePayloadType::InvRelayBlock,
+        KaspadMessagePayloadType::InvTransactions,
+        KaspadMessagePayloadType::Ping,
+        KaspadMessagePayloadType::Pong,
+        // KaspadMessagePayloadType::Verack,
+        // KaspadMessagePayloadType::Version,
+        KaspadMessagePayloadType::TransactionNotFound,
+        KaspadMessagePayloadType::Reject,
+        KaspadMessagePayloadType::PruningPointUtxoSetChunk,
+        KaspadMessagePayloadType::RequestIbdBlocks,
+        KaspadMessagePayloadType::UnexpectedPruningPoint,
+        KaspadMessagePayloadType::IbdBlockLocator,
+        KaspadMessagePayloadType::IbdBlockLocatorHighestHash,
+        KaspadMessagePayloadType::RequestNextPruningPointUtxoSetChunk,
+        KaspadMessagePayloadType::DonePruningPointUtxoSetChunks,
+        KaspadMessagePayloadType::IbdBlockLocatorHighestHashNotFound,
+        KaspadMessagePayloadType::BlockWithTrustedData,
+        KaspadMessagePayloadType::DoneBlocksWithTrustedData,
+        KaspadMessagePayloadType::RequestPruningPointAndItsAnticone,
+        KaspadMessagePayloadType::BlockHeaders,
+        KaspadMessagePayloadType::RequestNextHeaders,
+        KaspadMessagePayloadType::DoneHeaders,
+        KaspadMessagePayloadType::RequestPruningPointUtxoSet,
+        KaspadMessagePayloadType::RequestHeaders,
+        KaspadMessagePayloadType::RequestBlockLocator,
+        KaspadMessagePayloadType::PruningPoints,
+        KaspadMessagePayloadType::RequestPruningPointProof,
+        KaspadMessagePayloadType::PruningPointProof,
+        // KaspadMessagePayloadType::Ready,
+        KaspadMessagePayloadType::BlockWithTrustedDataV4,
+        KaspadMessagePayloadType::TrustedData,
+        KaspadMessagePayloadType::RequestIbdChainBlockLocator,
+        KaspadMessagePayloadType::IbdChainBlockLocator,
+        KaspadMessagePayloadType::RequestAntipast,
+        KaspadMessagePayloadType::RequestNextPruningPointAndItsAnticoneBlocks,
+    ])
 }
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,125 @@
 mod address_flow;
 
-use crate::address_flow::AddressesFlowInitializer;
-use clap::Parser;
-use kaspa_p2p_lib::Hub;
-use std::env;
+use crate::address_flow::{Initializer, ROUTER};
+use chrono::{DateTime, Utc};
+use clap::{Parser, ValueEnum};
+use kaspa_p2p_lib::pb::kaspad_message::Payload;
+use kaspa_p2p_lib::pb::{KaspadMessage, RequestAddressesMessage};
+use kaspa_p2p_lib::{make_message, Hub, Router};
+use kaspa_utils::hex::ToHex;
+use kaspa_utils::networking::IpAddress;
+use serde::Serialize;
+use std::collections::HashSet;
+use std::net::IpAddr;
 use std::sync::Arc;
 use std::time::Duration;
+use tokio::sync::mpsc;
+use tokio::sync::mpsc::Receiver;
+
+#[derive(ValueEnum, Clone, Debug)]
+enum RequestType {
+    Version,
+    Addresses,
+}
 
 #[derive(Parser)]
 struct Cli {
-    #[clap(short = 's', long, help = "The url to a kaspad instance, e.g 'localhost:16111'")]
+    #[clap(short = 's', long, default_value = "localhost:16111", help = "The ip:port of a kaspad instance")]
     url: String,
-    #[clap(short, long)]
-    verbose: Option<bool>,
-    #[clap(short, long)]
-    timeout: Option<u64>,
-    #[clap(short, long, default_value = "info", help = "error, warn, info, debug, trace, off")]
-    pub log_level: String,
-    #[clap(long, help = "Disable colored output")]
-    pub log_no_color: bool,
+    #[clap(short, long, default_value = "mainnet", help = "The network type and suffix, e.g. 'testnet-11'")]
+    pub network: String,
+    #[clap(value_enum, help = "Request type")]
+    pub request: RequestType,
+}
+
+#[derive(Eq, PartialEq, Hash, Serialize)]
+struct Version {
+    pub protocol_version: u32,
+    pub network: String,
+    pub services: u64,
+    pub timestamp: Option<DateTime<Utc>>,
+    pub address: Option<NetAddress>,
+    pub id: String,
+    pub user_agent: String,
+    pub disable_relay_tx: bool,
+    pub subnetwork_id: Option<String>,
+}
+
+#[derive(Eq, PartialEq, Hash, Serialize)]
+struct NetAddress {
+    ip: IpAddr,
+    port: u16,
 }
 
 #[tokio::main]
 async fn main() {
-    let cli_args = Cli::parse();
+    let cli_args = Arc::new(Cli::parse());
 
-    env::set_var("RUST_LOG", &cli_args.log_level);
-    env::set_var("RUST_LOG_STYLE", if cli_args.log_no_color { "never" } else { "always" });
-    env_logger::builder().target(env_logger::Target::Stdout).format_target(false).format_timestamp_millis().init();
-
-    let initializer = Arc::new(AddressesFlowInitializer::new());
+    let (sender, mut receiver) = mpsc::channel(10);
+    let initializer = Arc::new(Initializer::new(cli_args.clone(), sender));
     let adaptor = kaspa_p2p_lib::Adaptor::client_only(Hub::new(), initializer, Default::default());
-    let _peer_key = adaptor.connect_peer_with_retries(cli_args.url, 16, Duration::from_secs(1)).await;
-    tokio::time::sleep(Duration::from_secs(5)).await;
+
+    if let Err(e) = adaptor.connect_peer_with_retries(cli_args.url.clone(), 3, Duration::from_secs(1)).await {
+        panic!("Failed to connect to {}: {:?}", cli_args.url, e);
+    }
+
+    let router = ROUTER.read().unwrap().clone().unwrap();
+
+    match cli_args.request {
+        RequestType::Version => req_version(&mut receiver).await,
+        RequestType::Addresses => req_addresses(&mut receiver, router).await,
+    }
     adaptor.terminate_all_peers().await;
+}
+
+async fn req_version(receiver: &mut Receiver<KaspadMessage>) {
+    loop {
+        if let Some(msg) = receiver.recv().await {
+            if let Some(Payload::Version(version_msg)) = msg.payload {
+                let version = Version {
+                    protocol_version: version_msg.protocol_version,
+                    network: version_msg.network,
+                    services: version_msg.services,
+                    timestamp: DateTime::from_timestamp_millis(version_msg.timestamp),
+                    address: version_msg
+                        .address
+                        .and_then(|a| a.try_into().ok())
+                        .map(|(ip, port)| NetAddress { ip: ip.to_canonical(), port }),
+                    id: version_msg.id.to_hex(),
+                    user_agent: version_msg.user_agent,
+                    disable_relay_tx: version_msg.disable_relay_tx,
+                    subnetwork_id: version_msg.subnetwork_id.map(|s| s.bytes.to_hex()),
+                };
+                let json = serde_json::to_string(&version).unwrap();
+                println!("{}", json);
+                break;
+            }
+        }
+    }
+}
+
+async fn req_addresses(receiver: &mut Receiver<KaspadMessage>, router: Arc<Router>) {
+    let _ = router
+        .enqueue(make_message!(
+            Payload::RequestAddresses,
+            RequestAddressesMessage { include_all_subnetworks: false, subnetwork_id: None }
+        ))
+        .await;
+
+    loop {
+        if let Some(msg) = receiver.recv().await {
+            if let Some(Payload::Addresses(addresses_msg)) = msg.payload {
+                let mut addresses = HashSet::new();
+                for address in addresses_msg.address_list {
+                    if let Ok(result) = address.try_into() {
+                        let (ip, port): (IpAddress, u16) = result;
+                        addresses.insert(NetAddress { ip: ip.to_canonical(), port });
+                    }
+                }
+                let json = serde_json::to_string(&addresses).unwrap();
+                println!("{}", json);
+                break;
+            }
+        }
+    }
 }
